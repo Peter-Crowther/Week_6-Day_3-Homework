092902c38f47f37d5109d301d198193c7054e761
Peter Crowther <petercrowther90@hotmail.com>
2017-11-01 16:42:31 +0000

    created kaiju abstract class

---
+++ app/src/main/java/com/example/petes/homework/monsters/Kaiju.java
@@ -0,0 +1,38 @@
+package com.example.petes.homework.monsters;
+
+/**
+ * Created by petes on 01/11/2017.
+ */
+
+public abstract class Kaiju {
+    String name;
+    int healthValue;
+    int attackValue;
+    abstract String roar;
+
+    public Kaiju(String name, int healthValue, int attackValue) {
+        this.name = name;
+        this.healthValue = healthValue;
+        this.attackValue = attackValue;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealthValue() {
+        return this.healthValue;
+    }
+
+    public int getAttackValue() {
+        return this.attackValue;
+    }
+
+    
+    public String roar(){
+        return "Roar!";
+    }
+
+}
+
+
---
+++ app/src/test/java/com/example/petes/homework/KaijuTest.java
@@ -0,0 +1,8 @@
+package com.example.petes.homework;
+
+/**
+ * Created by petes on 01/11/2017.
+ */
+
+public class KaijuTest {
+}
